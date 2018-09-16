19df7f9ef3120f34a8fa8a4f329ffeee2cb2965e
HAYK00 <38961035+HAYK00@users.noreply.github.com>
2018-09-16 19:34:55 +0400

    Update send.php

--- send.php
+++ send.php
@@ -4,9 +4,9 @@ if($_POST['submit']) {
         $title = substr(htmlspecialchars(trim($_POST['title'])), 0, 1000); 
         $mess =  substr(htmlspecialchars(trim($_POST['mess'])), 0, 1000000); 
         // $to - кому отправляем 
-        $to = 'test@test.ru'; 
+        $to = 'hqyk2004@gmail.com'; 
         // $from - от кого 
-        $from='test@test.ru'; 
+        $from='joker98989898@mail.ru'; 
         // функция, которая отправляет наше письмо
         mail($to, $title, $mess, 'From:'.$from); 
         echo 'Спасибо! Ваше письмо отправлено.';
