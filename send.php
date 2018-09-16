010ecfa3a90ca1e3812a4feca42ad736f6ac7100
HAYK00 <38961035+HAYK00@users.noreply.github.com>
2018-09-16 19:37:11 +0400

    Update send.php

--- send.php
+++ send.php
@@ -1,17 +1,17 @@
-<?php 
-// если была нажата кнопка "Отправить" 
-if($_POST['submit']) {
-        $title = substr(htmlspecialchars(trim($_POST['title'])), 0, 1000); 
-        $mess =  substr(htmlspecialchars(trim($_POST['mess'])), 0, 1000000); 
-        // $to - кому отправляем 
-        $to = 'hqyk2004@gmail.com'; 
-        // $from - от кого 
-        $from='joker98989898@mail.ru'; 
-        // функция, которая отправляет наше письмо
-        mail($to, $title, $mess, 'From:'.$from); 
-        echo 'Спасибо! Ваше письмо отправлено.';
-} 
-else {
-        echo 'pix';
-}
-?>
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("joker98989898@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
